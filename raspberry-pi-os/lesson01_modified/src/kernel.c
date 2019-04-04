882f71c5f74e02888b718e5c0bf7147c23fc1e88
CalvinKrist <calvin.krist@yahoo.com>
2019-04-04 10:57:53 -0400

    added assembly function for getting proc id

--- raspberry-pi-os/lesson01_modified/src/kernel.c
+++ raspberry-pi-os/lesson01_modified/src/kernel.c
@@ -1,10 +1,16 @@
 #include "mini_uart.h"
 
-void kernel_main(char * args)
+void kernel_main(void)
 {
 	uart_init();
 	uart_send_string("Hello, world from processor ");
-	uart_send(args[0]);
+	
+	unsigned int cpuId = getCPUId();
+	if(cpuId == 0) 
+		uart_send('1');
+	else
+		uart_send('n');
+	uart_send('\r\n')
 
 	while (1) {
 		uart_send(uart_recv());
